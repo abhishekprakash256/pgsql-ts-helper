4d39582611a48673213466dd2716d58c0ef95c37
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-02-07 14:54:39 -0500

    first commit

---
+++ dist/connector_sql.js
@@ -0,0 +1,18 @@
+"use strict";
+/*
+The function to make the connection with postgresql
+
+*/
+exports.__esModule = true;
+exports.client = void 0;
+var pg_1 = require("pg");
+var client = new pg_1.Client({
+    user: 'abhi',
+    host: 'localhost',
+    database: 'contact',
+    password: 'mypassword',
+    port: 5432
+});
+exports.client = client;
+client.connect()
+    .then(function () { return console.log("Connected to PostgreSQL"); })["catch"](function (err) { return console.error(" Database Connection Error:", err); });
