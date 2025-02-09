b0e5950374a1d6d57fc84b322cca56ca5228a30f
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-02-08 23:28:48 -0500

    code

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.greet = void 0;
+// src/index.ts
+const greet = (name) => {
+    return `Hello, ${name}!`;
+};
+exports.greet = greet;
+//# sourceMappingURL=index.js.map
