4e9280c7ffa7057f058634bcaaa741d63d565165
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-02-18 23:56:05 -0500

    exports fixed

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
-// src/index.ts
-export { Helper_Fun } from './crud_operartions';
-export { client } from './connector_sql';
+/*
+The index file for the exports 
+*/
+
+export { Helper_Fun } from './crud_operartions'; // the helper_fun for all the sql methods 
+export { client } from './connector_sql';  // the sql client 
+
