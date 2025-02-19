3ebef589b776ed233f964677b45fdc40edb0d7d2
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-02-19 00:27:41 -0500

    package build fixed

--- node_modules/pgsql-ts-helper/dist/index.js
+++ node_modules/pgsql-ts-helper/dist/index.js
@@ -1,9 +1,11 @@
 "use strict";
+/*
+The index file for the exports
+*/
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.greet = void 0;
-// src/index.ts
-const greet = (name) => {
-    return `Hello, ${name}!`;
-};
-exports.greet = greet;
+exports.client = exports.Helper_Fun = void 0;
+var crud_operartions_1 = require("./crud_operartions"); // the helper_fun for all the sql methods 
+Object.defineProperty(exports, "Helper_Fun", { enumerable: true, get: function () { return crud_operartions_1.Helper_Fun; } });
+var connector_sql_1 = require("./connector_sql"); // the sql client 
+Object.defineProperty(exports, "client", { enumerable: true, get: function () { return connector_sql_1.client; } });
 //# sourceMappingURL=index.js.map
---
+++ node_modules/pgsql-ts-helper/test/test_package.js
@@ -0,0 +1,5 @@
+"use strict";
+/*
+The file to test the package
+*/
+Object.defineProperty(exports, "__esModule", { value: true });
